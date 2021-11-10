817ef9a983fadf56e8dc123bedeed4e6fdd30588
Gerald Goudy <ggoudy@outlook.com>
2021-11-10 14:21:34 -0500

    While loop

---
+++ Learning Python/Loops and Statements/while loop.py
@@ -0,0 +1,13 @@
+# The while loop will run as long as count equals 1
+# Since line1 equals string "M", the word Defenders
+# will print. count - 1 will decrement count to 0. 
+# The while loop will check that the condition is true
+# and since it's no longer true, the while loop will break
+
+line1 = "M"
+count = 1
+
+while count == 1:
+    if line1 == "M":
+        print("Defenders")
+        count = count - 1
