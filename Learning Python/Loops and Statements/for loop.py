676cc9f0289675088c69a7847d4ab25354bb4632
Gerald Goudy <ggoudy@outlook.com>
2021-11-10 13:49:21 -0500

    For loop and nested for loop example.

---
+++ Learning Python/Loops and Statements/for loop.py
@@ -0,0 +1,14 @@
+num = int(input("Please enter a number: "))
+
+# For loop that prints 5 times 5 using a range defined
+# by the input above.
+for name in range(0,num):
+    print(5*5)
+
+# For loop that runs 2 times and contains a nested for loop 
+# that prints the word cyber 3 times and then prints the 
+# word defenders.
+for letter in range(0,2):
+    for lett in range(3):
+            print("Cyber")
+    print("Defenders")
