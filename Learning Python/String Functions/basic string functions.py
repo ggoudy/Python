55f0ce6a7a222118514f4e5bfb9ecd69d24e8db8
Gerald Goudy <ggoudy@outlook.com>
2021-11-10 23:07:54 -0500

    String manipulation examples

---
+++ Learning Python/String Functions/basic string functions.py
@@ -0,0 +1,26 @@
+word = "CyberDefenders"
+word2 = "cybersecurity"
+
+# Counts the number of times 'e' occurs
+print(word.count("e"))
+
+# Finds 'n' and returns the index (location in string)
+print(word.find("n"))
+
+# Checks if the string ends with 's'
+print(word.endswith("s"))
+
+# Splits the word into 2 starting at the character 'D'
+print(word.split("D"))
+
+# Separates the word based on the character 'r'
+print(word.partition("r"))
+
+# Sets the entire string to lowercase
+print(str.lower(word))
+
+# Sets the entire string to uppercase
+print(str.upper(word))
+
+# Capitializes the first character of the string
+print(str.capitalize(word2))
