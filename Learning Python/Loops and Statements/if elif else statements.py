8c4fd475298c997b5a1315da7adbd22347056120
Gerald Goudy <ggoudy@outlook.com>
2021-11-10 22:51:01 -0500

    Utilized str.capitalize to replace or operators

--- Learning Python/Loops and Statements/if elif else statements.py
+++ Learning Python/Loops and Statements/if elif else statements.py
@@ -1,8 +1,20 @@
 word = input("Please enter the word Cyber or Defenders: ")
 
+# Slightly more elegant solution
+if str.capitalize(word) == "Cyber":
+    print("Welcome Cyber!")
+elif str.capitalize(word) == "Defenders":
+    print("Welcome Defenders!")
+else:
+    print("Unauthorized")
+
+# Original solution
+'''
 if word == "Cyber" or word == "cyber":
     print("Welcome Cyber!")
 elif word == "Defenders" or word == "defenders":
     print("Welcome Defenders!")
 else:
     print("Unauthorized")
+'''
+
