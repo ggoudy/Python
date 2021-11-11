afdf82129f98be577d1daa76816c76dc2f2f1976
Gerald Goudy <ggoudy@outlook.com>
2021-11-10 22:14:55 -0500

    if elif else statement examples

---
+++ Learning Python/Loops and Statements/if elif else statements.py
@@ -0,0 +1,8 @@
+word = input("Please enter the word Cyber or Defenders: ")
+
+if word == "Cyber" or word == "cyber":
+    print("Welcome Cyber!")
+elif word == "Defenders" or word == "defenders":
+    print("Welcome Defenders!")
+else:
+    print("Unauthorized")
